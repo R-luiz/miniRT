cd0ce7056bbafe314a28db3344a7f72ee00fbf24
Robin Luiz <rluiz@c2r4p3.42lehavre.fr>
2024-02-03 18:10:19 +0100

    starting of camera moving

--- src/camera/camera.c
+++ src/camera/camera.c
@@ -6,21 +6,21 @@
 /*   By: rluiz <rluiz@student.42lehavre.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 18:18:03 by liguyon           #+#    #+#             */
-/*   Updated: 2024/02/03 17:37:59 by rluiz            ###   ########.fr       */
+/*   Updated: 2024/02/03 17:51:55 by rluiz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./camera.h"
 #include "canvas/canvas.h"
 #include "engine/engine.h"
-#include "ray.h"
 #include "libft.h"
+#include "ray.h"
 #include <math.h>
 #include <stddef.h>
 #include <stdio.h>
 
-void		camera_init_viewport(
-	t_camera *cam, int canvas_width, int canvas_height, void *arena)
+void	camera_init_viewport(t_camera *cam, int canvas_width, int canvas_height,
+		void *arena)
 {
 	float		focal_length;
 	t_vec3		cam_u;
@@ -42,13 +42,12 @@ void		camera_init_viewport(
 	vp.upper_left = vec3_add(cam->center, vec3_mul(cam_w, focal_length));
 	vp.upper_left = vec3_sub(vp.upper_left, vec3_div(vp.u, 2));
 	vp.upper_left = vec3_sub(vp.upper_left, vec3_div(vp.v, 2));
-	cam->vp->pixel_00 = vec3_add(
-		vp.upper_left, vec3_mul(
-			vec3_add(cam->vp->pixel_du, cam->vp->pixel_dv), 0.5f));
+	cam->vp->pixel_00 = vec3_add(vp.upper_left,
+			vec3_mul(vec3_add(cam->vp->pixel_du, cam->vp->pixel_dv), 0.5f));
 }
 
-t_camera	*camera_create(
-	t_point3 center, t_vec3 direction, float hfov, void *arena)
+t_camera	*camera_create(t_point3 center, t_vec3 direction, float hfov,
+		void *arena)
 {
 	t_camera	*cam;
 
@@ -61,82 +60,122 @@ t_camera	*camera_create(
 
 bool	hit_sphere(t_point3 center, float radius, t_ray *ray)
 {
-	t_vec3	oc = vec3_sub(ray->origin, center);
-	float a = vec3_dot(ray->direction, ray->direction);
-	float b = 2 * vec3_dot(oc, ray->direction);
-	float c = vec3_dot(oc, oc) - radius * radius;
-	float discriminant = b * b - 4 * a * c;
+	t_vec3	oc;
+	float	a;
+	float	b;
+	float	c;
+	float	discriminant;
+
+	oc = vec3_sub(ray->origin, center);
+	a = vec3_dot(ray->direction, ray->direction);
+	b = 2 * vec3_dot(oc, ray->direction);
+	c = vec3_dot(oc, oc) - radius * radius;
+	discriminant = b * b - 4 * a * c;
 	return (discriminant >= 0);
 }
 
 float	hit_sphere_distance(t_point3 center, float radius, t_ray *ray)
 {
-	t_vec3	oc = vec3_sub(ray->origin, center);
-	float a = vec3_dot(ray->direction, ray->direction);
-	float b = 2 * vec3_dot(oc, ray->direction);
-	float c = vec3_dot(oc, oc) - radius * radius;
-	float discriminant = b * b - 4 * a * c;
+	t_vec3	oc;
+	float	a;
+	float	b;
+	float	c;
+	float	discriminant;
+
+	oc = vec3_sub(ray->origin, center);
+	a = vec3_dot(ray->direction, ray->direction);
+	b = 2 * vec3_dot(oc, ray->direction);
+	c = vec3_dot(oc, oc) - radius * radius;
+	discriminant = b * b - 4 * a * c;
 	if (discriminant < 0)
 		return (-1);
 	return (-b - sqrtf(discriminant)) / (2 * a);
 }
 
+t_vec3	calc_spheres(t_render *rd, int i, int j)
+{
+	t_camera	*camera;
+	t_canvas	*canvas;
+	t_objects	*objects;
+	t_point3	pixel_center;
+	t_vec3		ray_direction;
+	t_ray		ray;
+	t_list		*object;
+	float		min_distance;
+	t_vec3		final_color;
+	t_sphere	sphere;
+	float		distance;
+	t_vec3		hit_point;
+	t_vec3		normal;
+	float		t;
+
+	camera = rd->camera;
+	canvas = rd->canvas;
+	objects = rd->objects;
+	pixel_center = vec3_add(camera->vp->pixel_00,
+			vec3_add(vec3_mul(camera->vp->pixel_du, (float)i),
+				vec3_mul(camera->vp->pixel_dv, (float)j)));
+	ray_direction = vec3_normalize(vec3_sub(pixel_center, camera->center));
+	ray = (t_ray){.origin = camera->center, .direction = ray_direction};
+	min_distance = INFINITY;
+	final_color = (t_vec3){0, 0, 0};
+	object = objects->spheres;
+	for (int s = 0; s < objects->sp_count; s++)
+	{
+		sphere = *(t_sphere *)object->data;
+		distance = hit_sphere_distance(sphere.center, sphere.diameter / 2,
+				&ray);
+		if (distance > 0.0f && distance < min_distance)
+		{
+			min_distance = distance;
+			hit_point = vec3_add(ray.origin, vec3_mul(ray.direction, distance));
+			normal = vec3_normalize(vec3_sub(hit_point, sphere.center));
+			final_color = vec3_mul(vec3_add(vec3_mul(normal, 0.5f),
+						(t_vec3){0.5f, 0.5f, 0.5f}), 255.0f);
+		}
+		object = object->next;
+	}
+	if (min_distance == INFINITY)
+	{
+		// Background gradient
+		t = 0.5f * (ray_direction.y + 1.0f);
+		final_color = vec3_add(vec3_mul((t_vec3){1.0f, 1.0f, 1.0f}, (1.0f - t)),
+				vec3_mul((t_vec3){0.5f, 0.7f, 1.0f}, t));
+		final_color = vec3_mul(final_color, 255.0f);
+	}
+	return (final_color);
+}
+
 void	*camera_render(void *vargp)
 {
-	t_render	*rd = (t_render *)vargp;
-	t_camera	*camera = rd->camera;
-	t_canvas	*canvas = rd->canvas;
-	t_objects	*objects = rd->objects;
-	int			i, j;
+	t_render	*rd;
+	t_camera	*camera;
+	t_canvas	*canvas;
+	t_objects	*objects;
 	t_color		color;
-	
+	t_vec3		final_color;
+
+	rd = (t_render *)vargp;
+	camera = rd->camera;
+	canvas = rd->canvas;
+	objects = rd->objects;
+	int i, j;
+	final_color = (t_vec3){0, 0, 0};
 	while (rd->engine->is_running)
 	{
-		for (j = 0; j < canvas->height; j++) {
-			for (i = 0; i < canvas->width; i++) {
-				if (!engine_is_running(rd->engine)) {
-					return NULL;
+		for (j = 0; j < canvas->height; j++)
+		{
+			for (i = 0; i < canvas->width; i++)
+			{
+				if (!engine_is_running(rd->engine))
+				{
+					return (NULL);
 				}
-
-				t_point3 pixel_center = vec3_add(
-					camera->vp->pixel_00, 
-					vec3_add(
-						vec3_mul(camera->vp->pixel_du, (float)i), 
-						vec3_mul(camera->vp->pixel_dv, (float)j)
-					)
-				);
-
-				t_vec3 ray_direction = vec3_normalize(vec3_sub(pixel_center, camera->center));
-				t_ray ray = { .origin = camera->center, .direction = ray_direction };
-
-				float min_distance = INFINITY;
-				t_vec3 final_color = {0, 0, 0};
-
-				for (int s = 0; s < objects->sp_count; s++) {
-					t_sphere sphere = *(t_sphere *)objects->spheres->data;
-					float distance = hit_sphere_distance(sphere.center, sphere.diameter/2, &ray);
-					if (distance > 0.0f && distance < min_distance) {
-						min_distance = distance;
-						t_vec3 hit_point = vec3_add(ray.origin, vec3_mul(ray.direction, distance));
-						t_vec3 normal = vec3_normalize(vec3_sub(hit_point, sphere.center));
-						final_color = vec3_mul(vec3_add(vec3_mul(normal, 0.5f), (t_vec3){0.5f, 0.5f, 0.5f}), 255.0f);
-					}
-				}
-
-				if (min_distance == INFINITY) {
-					// Background gradient
-					float t = 0.5f * (ray_direction.y + 1.0f);
-					final_color = vec3_add(
-						vec3_mul((t_vec3){1.0f, 1.0f, 1.0f}, (1.0f - t)),
-						vec3_mul((t_vec3){0.5f, 0.7f, 1.0f}, t)
-					);
-					final_color = vec3_mul(final_color, 255.0f);
-				}
-
+				final_color = calc_spheres(rd, i, j);
 				color = ((int)final_color.x << 16) | ((int)final_color.y << 8) | (int)final_color.z;
 				canvas_draw(canvas, i, j, color);
 			}
 		}
 	}
-	return NULL;
+	return (NULL);
 }
--- src/engine/inputs.c
+++ src/engine/inputs.c
@@ -6,7 +6,7 @@
 /*   By: rluiz <rluiz@student.42lehavre.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 14:44:00 by liguyon           #+#    #+#             */
-/*   Updated: 2024/02/03 17:40:06 by rluiz            ###   ########.fr       */
+/*   Updated: 2024/02/03 18:08:13 by rluiz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,24 +29,54 @@ static int	inputs_process_structure(t_engine *eng)
 	return (0);
 }
 
-static int	inputs_process_keypress(int keycode, t_render *rd)
-{
-	t_camera	*camera;
-	t_engine	*eng;
+static int inputs_process_keypress(int keycode, t_render *rd) {
+    t_camera *camera = rd->camera;
+    // Assuming a movement speed
+    float move_speed = 0.01f;
 
-	camera = rd->camera;
-	eng = rd->engine;
-	if (keycode == XK_Escape)
-		loop_end(eng);
-	if (keycode == XK_w)
-		camera->center.y += 0.02;
-	if (keycode == XK_a)
-		camera->center.x -= 0.02;
-	if (keycode == XK_s)
-		camera->center.y -= 0.02;
-	if (keycode == XK_d)
-		camera->center.x += 0.02;
-	return (0);
+    if (keycode == XK_Escape)
+        loop_end(rd->engine);
+    if (keycode == XK_w)
+        camera->center.y -= move_speed; // Moving up
+    if (keycode == XK_a)
+        camera->center.x += move_speed; // Moving left
+    if (keycode == XK_s)
+        camera->center.y += move_speed; // Moving down
+    if (keycode == XK_d)
+        camera->center.x -= move_speed; // Moving right
+    return 0;
+}
+
+
+static int inputs_process_mouse(int button, int x, int y, t_render *rd) {
+    t_camera *camera = rd->camera;
+    // Assuming zoom speed
+    float zoom_speed = 0.05f;
+    float aspect_ratio = (float)rd->canvas->width / (float)rd->canvas->height;
+
+    // Calculate normalized device coordinates (NDC) of the mouse position
+    float ndc_x = (2.0f * x) / rd->canvas->width - 1.0f;
+    float ndc_y = 1.0f - (2.0f * y) / rd->canvas->height; // Invert y-axis
+
+	ndc_x *= -1;
+	ndc_y *= -1;
+    // Convert NDC to world coordinates - assuming a simple perspective projection
+    t_vec3 ray_dir = vec3_normalize((t_vec3){
+        ndc_x * aspect_ratio, // Adjust for aspect ratio
+        ndc_y,
+        -1.0f // Assume the camera looks towards -z in NDC
+    });
+
+    // Zoom in or out based on the mouse wheel input
+    if (button == 4) {
+        // Zoom in: Move the camera slightly towards the calculated direction
+        camera->center = vec3_add(camera->center, vec3_mul(ray_dir, zoom_speed));
+    } else if (button == 5) {
+        // Zoom out: Move the camera slightly away from the calculated direction
+        camera->center = vec3_sub(camera->center, vec3_mul(ray_dir, zoom_speed));
+    }
+
+    return 0;
 }
 
 void	inputs_bind(t_render *rd)
@@ -56,4 +86,6 @@ void	inputs_bind(t_render *rd)
 		inputs_process_structure, eng);
 	mlx_hook(eng->grph->win_ptr, KeyPress, KeyPressMask,
 		inputs_process_keypress, rd);
+	//mouse hook for zooming
+	mlx_hook(eng->grph->win_ptr, ButtonPress, ButtonPressMask, inputs_process_mouse, rd);
 }
