5caad3f229ad434572b95fccef787fcb2db2cfab
Valentin Malassigne-Lepicard <vmalassi@c2r5p4.42lehavre.fr>
2024-03-29 19:07:48 +0100

    fix

--- src/objects/distance_funcs.c
+++ src/objects/distance_funcs.c
@@ -6,7 +6,7 @@
 /*   By: vmalassi <vmalassi@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 17:05:14 by rluiz             #+#    #+#             */
-/*   Updated: 2024/03/29 11:35:46 by vmalassi         ###   ########.fr       */
+/*   Updated: 2024/03/29 19:00:12 by vmalassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,7 @@ float	hit_cylinder_distance(t_object *cylinder, t_lightray ray)
 	t_cylinder	*cy;
 	t_vec3		co;
 	t_vec3		d;
-	t_vec3		a;
+	t_vec3		a_vec;
 	t_vec3		a_perp;
 	t_vec3		b_perp;
 	t_plane		topcap;
@@ -76,10 +76,10 @@ float	hit_cylinder_distance(t_object *cylinder, t_lightray ray)
 	cy = (t_cylinder *)cylinder;
 	co = vec3_sub(ray.origin, cy->center); // Vector from cylinder center to ray origin
 	d = ray.direction; // Ray direction vector
-	a = vec3_normalize(cy->normal); // Cylinder axis direction
+	a_vec = vec3_normalize(cy->normal); // Cylinder axis direction
 
-	a_perp = vec3_sub(d, vec3_mul(a, vec3_dot(d, a)));
-	b_perp = vec3_sub(co, vec3_mul(a, vec3_dot(co, a)));
+	a_perp = vec3_sub(d, vec3_mul(a_vec, vec3_dot(d, a_vec)));
+	b_perp = vec3_sub(co, vec3_mul(a_vec, vec3_dot(co, a_vec)));
 
 	// Quadratic coefficients for intersection with cylindrical surface
 	a = vec3_dot(a_perp, a_perp);
@@ -97,17 +97,17 @@ float	hit_cylinder_distance(t_object *cylinder, t_lightray ray)
 		t0 = (-b - sqrt_discriminant) / (2 * a);
 		t1 = (-b + sqrt_discriminant) / (2 * a);
 		// Check if the intersections are within the height bounds of the cylinder
-		y0 = vec3_dot(vec3_add(co, vec3_mul(d, t0)), a);
-		y1 = vec3_dot(vec3_add(co, vec3_mul(d, t1)), a);
+		y0 = vec3_dot(vec3_add(co, vec3_mul(d, t0)), a_vec);
+		y1 = vec3_dot(vec3_add(co, vec3_mul(d, t1)), a_vec);
 		if ((y0 >= 0 && y0 <= cy->height) && t0 > 0)
 			t_min = fmin(t_min, t0);
 		if ((y1 >= 0 && y1 <= cy->height) && t1 > 0)
 			t_min = fmin(t_min, t1);
 	}
-	topcap.apoint = vec3_add(cy->center, vec3_mul(a, cy->height));
-	topcap.normal = a;
+	topcap.apoint = vec3_add(cy->center, vec3_mul(a_vec, cy->height));
+	topcap.normal = a_vec;
 	botcap.apoint = cy->center;
-	botcap.normal = vec3_mul(a, -1);
+	botcap.normal = vec3_mul(a_vec, -1);
 	denom = vec3_dot(d, topcap.normal);
 	if (fabs(denom) > 0.0001)
 	{
